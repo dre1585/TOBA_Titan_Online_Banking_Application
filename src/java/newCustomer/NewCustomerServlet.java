1099c7ce1d973687f5584411506b48d514a8eb7f
gaine_000 <gaine_000@DRE>
2017-09-20 17:25:23 -0400

    new commit

--- src/java/newCustomer/NewCustomerServlet.java
+++ src/java/newCustomer/NewCustomerServlet.java
@@ -5,15 +5,16 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
 
 public class NewCustomerServlet extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        String url = "/new_customer.jsp";
-        getServletContext().getRequestDispatcher(url)
-                .forward(request, response);
+        String url = "";
+       
 
         String fname = request.getParameter("fname");
         String lname = request.getParameter("fname");
@@ -22,29 +23,34 @@ public class NewCustomerServlet extends HttpServlet {
         String city = request.getParameter("city");
         String zipcode = request.getParameter("zipcode");
         String email = request.getParameter("email");
+        String lastname = request.getParameter("lastname");
 
-        User user = new User(fname, lname, phone,address,
-                             city, zipcode, email);
-        request.setAttribute("user", user);
+        String username = lname + zipcode;
+        String tempPass = "welcome1";
 
         //validate the parameters
-        
         String message;
         if (fname == null || lname == null || phone == null || address == null || city == null || zipcode == null || email == null
                 || fname.isEmpty() || lname.isEmpty() || phone.isEmpty() || address.isEmpty() || city.isEmpty() || zipcode.isEmpty() || email.isEmpty()) {
             message = "Please fill all text boxes";
             url = "/new_customer.jsp";
         } else {
+            User user = new User(fname, lname, phone, address,
+                    city, zipcode, email, username, tempPass);
+            HttpSession session = request.getSession();
+            session.setAttribute("user", user);
+            url = "/Success.jsp";
             //populate the user object
             //save it in the session
             //*url = "/Success.jsp";
-            response.sendRedirect("/Success.jsp");
+            //response.sendRedirect("/Success.jsp");
             //UserDB.insert(user);
+            
         }
-        //tell the context to forward to the correct page based on the url
-        getServletContext()
-                .getRequestDispatcher(url)
+        
+        getServletContext().getRequestDispatcher(url)
                 .forward(request, response);
+      
     }
 
     @Override
