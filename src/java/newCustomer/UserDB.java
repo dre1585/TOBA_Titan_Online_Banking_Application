65cc3713219bc2e2899f067dd93b7b8249a7a41e
gaine_000 <gaine_000@DRE>
2017-09-27 16:42:55 -0400

    new commit

---
+++ src/java/newCustomer/UserDB.java
@@ -0,0 +1,21 @@
+package newCustomer;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+public class UserDB {
+
+    public static void insert(User user) {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        trans.begin();
+        try {
+            em.persist(user);
+            trans.commit();
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            em.close();
+        }
+    }
+}
